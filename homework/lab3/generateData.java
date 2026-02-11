548844f05b39a73ccc4d7d5852ceaaa7ccb511d4
Tripnaught <257220612+tripnaught@users.noreply.github.com>
2026-02-11 12:42:53 -0800

    generate testing data as csv
    part of week 3 lab

---
+++ homework/lab3/generateData.java
@@ -0,0 +1,90 @@
+package lab3;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class generateData {
+	// vs code won't let me use paths relative to java files for some reason,
+	// they have to be relative to the root of the workspace.
+
+	// when testing this on a different machine, you may need to change this path.
+	public static final String CSV_FILEPATH = "homework/lab3/data.csv";
+
+	public static Path path = Path.of(CSV_FILEPATH);
+
+	public static void main(String[] args) {
+		clearCSV();
+		fillCSV(10000);
+	}
+
+	public static void clearCSV() {
+		try (BufferedWriter bw = Files.newBufferedWriter(path)) {
+			bw.write("");
+			System.out.println("csv cleared!");
+		} catch (IOException e) {
+			System.err.format("error clearing data.csv");
+		}
+	}
+
+	public static void fillCSV(int rows) {
+		try (BufferedWriter bw = Files.newBufferedWriter(path, StandardOpenOption.APPEND)) {
+			// header row
+			bw.write("sale_id,sale_date,amount,product");
+			bw.newLine();
+
+			for (int i = 0; i < rows; i++) {
+				// sale ID
+				bw.write(Integer.toString(i));
+				bw.write(",");
+				
+				// sale date
+				bw.write(Integer.toString(randIntRange(1995, 2027)));
+				bw.write("-");
+				bw.write(Integer.toString(randIntRange(1, 13)));
+				bw.write("-");
+				bw.write(Integer.toString(randIntRange(1, 31)));
+				bw.write(",");
+				
+				// amount
+				bw.write(Integer.toString(randIntRange(0, 10000)));
+				bw.write(".");
+				bw.write(Integer.toString(randIntRange(0, 100)));
+				bw.write(",");
+
+				// product
+				String[] products = {
+					"gadget",
+					"gizmo",
+					"whoozit",
+					"whatsit",
+					"thingamabob",
+					"doohickey",
+					"whatchamacalit",
+					"thingy",
+					"book",
+					"car",
+					"toy",
+					"phone",
+					"pet",
+					"subscription",
+				};
+				bw.write(products[randIntRange(0, products.length)]);
+				
+				bw.newLine();
+			}
+
+			System.out.println("csv filled!");
+		} catch (IOException e) {
+			System.err.format("error writing to data.csv");
+		}
+	}
+
+	// this function comes from baeldung.com, though it's similar to
+	// one used in last semester's pokemon lab to generate random ints.
+	public static int randIntRange(int min, int max) {
+		return (int) ((Math.random() * (max - min)) + min);
+	}
+}
